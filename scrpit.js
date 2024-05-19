7df7bf469e77a154a089290c0ac0cdd040653d9e
Trynew19 <chauhankrishana42@gmail.com>
2024-05-19 11:01:38 +0530

    Select Lang complete

--- scrpit.js
+++ scrpit.js
@@ -9,3 +9,18 @@ var editor = CodeMirror.fromTextArea(document.getElementById("editor"),{
 var width = window.innerWidth
 
 editor.setSize(0.7*width,"500")
+
+var option = document.getElementById("inlineFormSelectPref")
+
+option.addEventListener("change",function(){
+
+    if (option.value == "Java" ) {
+        editor.setOption("mode","text/x-java")
+    }
+   else if (option.value == "Python" ) {
+        editor.setOption("mode","text/x-python")
+    }
+    else{
+        editor.setOption("mode","text/x-c++src")
+    }
+})
